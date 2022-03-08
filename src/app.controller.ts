91e1a9da906da41f8e50a361a80811e991b55354
alfonsoaranzazu <alfonso@viruett.com>
2022-03-08 10:03:31 -0800

    create proof response model

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ProofRequest } from './models/ProofRequest';
+import { ProofResponse } from './models/ProofResponse';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Post('/proof')
-  getProof(@Body() proofRequest: ProofRequest): string[] {
-    return this.appService.getProof(proofRequest.wallet);
+  getProof(@Body() proofRequest: ProofRequest): ProofResponse {
+    return {
+      proof: this.appService.getProof(proofRequest.wallet)
+    }
   }
 }
---
+++ src/models/ProofResponse.ts
@@ -0,0 +1,3 @@
+export interface ProofResponse {
+  proof: string[];
+}
