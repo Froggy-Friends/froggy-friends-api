2212b4056b429bd53fc5cdd1538fdb40f493b46b
alfonsoaranzazu <alfonso@viruett.com>
2022-03-08 10:02:31 -0800

    adds proof endpoint

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import MerkleTree from "merkletreejs";
+import { utils } from "ethers";
+import wallets from './wallets';
+const { keccak256 } = utils;
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  froggylist: MerkleTree;
+
+  constructor() {
+    this.froggylist = new MerkleTree(wallets.map(wallet => keccak256(wallet)), keccak256, { sortPairs: true });
+    console.log("froggylist root: ", this.froggylist.getHexRoot());
+  }
+
+  getProof(address: string): string[] {
+    return this.froggylist.getHexProof(keccak256(address));
   }
 }
---
+++ src/wallets.ts
@@ -0,0 +1,6 @@
+const wallets = [
+  "0x1AF8c7140cD8AfCD6e756bf9c68320905C355658",
+  "0xA46e71823371d2cC153a48eE2965Dd408a2EcF65"
+];
+
+export default wallets;
