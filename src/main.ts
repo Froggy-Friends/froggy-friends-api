0c5164ca929edce5262935baa1ffe551be97a152
alfonsoaranzazu <alfonso@viruett.com>
2022-04-28 23:18:44 -0700

    enable cors on staking website

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,16 @@ import { AppModule } from './app.module';
 import { NotFoundExceptionFilter } from './filters/NotFoundExceptionFilter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true});
+  const app = await NestFactory.create(
+    AppModule, 
+    { 
+      cors: {
+        origin: ["https://stake.froggyfriendsnft.com"],
+        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+        optionsSuccessStatus: 200
+      }
+    }
+  );
   app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(process.env.PORT || 8080);
 }
