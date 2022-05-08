bc8930d35cf701648c995a7043663e421a9b3dfc
alfonsoaranzazu <alfonso@viruett.com>
2022-05-07 23:42:05 -0700

    adds moralis

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoralisController } from './controllers/moralis.controller';
+import { MoralisService } from './services/moralis.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MoralisController],
+  providers: [AppService, MoralisService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,8 @@ import { AppModule } from './app.module';
 import { NotFoundExceptionFilter } from './filters/NotFoundExceptionFilter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule, 
-    { 
-      cors: {
-        origin: ["https://stake.froggyfriendsnft.com"],
-        methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-        optionsSuccessStatus: 200
-      }
-    }
-  );
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalFilters(new NotFoundExceptionFilter());
   await app.listen(process.env.PORT || 8080);
 }
