cc6baecd94db79ce0b9ece424f84c58738ff3d9e
alfonsoaranzazu <alfonso@viruett.com>
2022-10-13 03:11:50 -0700

    init leaderboard on server startup

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { NotFoundExceptionFilter } from './filters/NotFoundExceptionFilter';
 import Moralis from 'moralis';
 import * as dotenv from "dotenv";
+import { StakingService } from './services/staking.service';
 
 dotenv.config();
 const { MORALIS_KEY } = process.env;
@@ -15,6 +16,8 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.useGlobalFilters(new NotFoundExceptionFilter());
+    const stakingService = app.get(StakingService);
+    stakingService.initLeaderboard();
     
     // start server
     await app.listen(process.env.PORT || 8080);
