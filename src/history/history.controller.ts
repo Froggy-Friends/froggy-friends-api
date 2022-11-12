b3d5887cbf4c610c2ed7c262701abcc91940e761
alfonsoaranzazu <alfonso@viruett.com>
2022-11-11 22:51:11 -0800

    create history endpoint

---
+++ src/history/history.controller.ts
@@ -0,0 +1,15 @@
+import { HistoryService } from './history.service';
+import { Controller, Get, Param } from "@nestjs/common";
+import { History } from './history.entity';
+
+@Controller('history')
+export class HistoryController {
+  constructor(private readonly historyService: HistoryService) {
+
+  }
+
+  @Get('/:account')
+  async getAccountHistory(@Param('account') account: string): Promise<History[]> {
+    return this.historyService.findHistory(account);
+  }
+}
