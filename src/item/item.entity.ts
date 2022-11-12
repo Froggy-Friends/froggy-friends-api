a9b7c42977c6ebbd97471d2743537f1c71118cff
alfonsoaranzazu <alfonso@viruett.com>
2022-11-12 02:14:28 -0800

    creates item module

---
+++ src/item/item.entity.ts
@@ -0,0 +1,26 @@
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity({ name: 'Item', synchronize: false})
+export class Item {
+  @PrimaryColumn() id: number;
+  @Column() name: string;
+  @Column() description: string;
+  @Column() category: string;
+  @Column() image: string;
+  @Column() imageTransparent: string;
+  @Column() previewImage: string;
+  @Column() twitter: string;
+  @Column() discord: string;
+  @Column() website: string;
+  @Column() endDate: number;
+  @Column() collabId: number;
+  @Column() isCommunity: boolean;
+  @Column() isBoost: boolean;
+  @Column() isTrait: boolean;
+  @Column() isPhysical: boolean;
+  @Column() isAllowlist: boolean;
+  @Column() rarity: string;
+  @Column() boost: number;
+  @Column() friendOrigin: string;
+  @Column() traitLayer: string;
+}
---
+++ src/item/item.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Not, Repository } from "typeorm";
+import { Item } from "./item.entity";
+import { ItemBooleans } from './ItemBooleans';
+import { ItemNumbers } from './ItemNumbers';
+import { ItemStrings } from './ItemStrings';
+
+@Injectable()
+export class ItemService {
+  
+  constructor(@InjectRepository(Item) private itemRepo: Repository<Item>) {
+
+  }
+
+  getItems(id: number): Promise<Item[]> {
+   return this.itemRepo.find({ 
+      where: { 
+        id: Not(null)
+      }
+    });
+  }
+
+  async saveItem(
+    itemNumbers: ItemNumbers,
+    itemStrings: ItemStrings,
+    itemBooleans: ItemBooleans
+  ): Promise<Item> {
+    const item = new Item();
+    item.endDate = itemNumbers.endDate;
+    item.collabId = itemNumbers.collabId;
+    item.boost = itemNumbers.boost;
+    item.name = itemStrings.name;
+    item.description = itemStrings.description;
+    item.category = itemStrings.category;
+    item.image = itemStrings.image;
+    item.imageTransparent = itemStrings.imageTransparent;
+    item.previewImage = itemStrings.previewImage;
+    item.twitter = itemStrings.twitter;
+    item.discord = itemStrings.discord;
+    item.website = itemStrings.website;
+    item.rarity = itemStrings.rarity;
+    item.friendOrigin = itemStrings.friendOrigin;
+    item.traitLayer = itemStrings.traitLayer;
+    item.isCommunity = itemBooleans.isCommunity;
+    item.isBoost = itemBooleans.isBoost;
+    item.isTrait = itemBooleans.isTrait;
+    item.isPhysical = itemBooleans.isPhysical;
+    item.isAllowlist = itemBooleans.isAllowlist;
+    return await this.itemRepo.save(item);
+  }
+
+  
+}
