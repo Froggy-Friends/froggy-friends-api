29a9647703aaf90b8f90038c28161fe71d6d5c2c
alfonsoaranzazu <alfonso@viruett.com>
2022-11-14 10:29:55 -0800

    adds constructor

--- src/spaces/spaces.controller.ts
+++ src/spaces/spaces.controller.ts
@@ -1,6 +1,10 @@
 import { Controller } from "@nestjs/common";
+import { SpacesService } from "./spaces.service";
 
 @Controller("/spaces")
 export class SpacesController {
-  
+
+  constructor(private readonly spacesService: SpacesService) {
+    
+  }
 }
