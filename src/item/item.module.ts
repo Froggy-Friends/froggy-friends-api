d225147ee52316d5564ec033a29e0f46482a4599
alfonsoaranzazu <alfonso@viruett.com>
2023-01-06 19:35:28 -0800

    remove unused code

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -2,7 +2,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from "@nestjs/common";
 import { Item } from './item.entity';
 import { ItemService } from './item.service';
-import { ContractService } from 'src/contract/contract.service';
 import { ContractModule } from 'src/contract/contract.module';
 
 @Module({
