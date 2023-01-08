321bb5255f76a23b9f59f00640780b40cade1892
alfonsoaranzazu <alfonso@viruett.com>
2023-01-08 01:50:34 -0800

    adds optional items constructor

--- src/item/item.entity.ts
+++ src/item/item.entity.ts
@@ -28,4 +28,35 @@ export class Item {
   @Column() supply: number;
   @Column() walletLimit: number;
   @Column() isOnSale: boolean;
+
+  constructor(item?: any) {
+    if (item) {
+      this.id = item.itemId;
+      this.name = item.name;
+      this.description = item.description;
+      this.category = item.category;
+      this.image = item.image;
+      this.imageTransparent = item.imageTransparent;
+      this.twitter = item.twitter;
+      this.discord = item.discord;
+      this.website = item.website;
+      this.endDate = item.endDate;
+      this.collabId = item.collabId;
+      this.isCommunity = item.isCommunity;
+      this.isBoost = item.isBoost;
+      this.isTrait = item.isTrait;
+      this.isPhysical = item.isPhysical;
+      this.isAllowlist = item.isAllowlist;
+      this.rarity = item.rarity;
+      this.boost = item.boost;
+      this.friendOrigin = item.friendOrigin;
+      this.traitLayer = item.traitLayer;
+      this.price = item.price;
+      this.percent = item.percent;
+      this.minted = item.minted;
+      this.supply = item.supply;
+      this.walletLimit = item.walletLimit;
+      this.isOnSale = item.isOnSale;
+    }
+  }
 }
