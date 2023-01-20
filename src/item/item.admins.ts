32d796a59b2b3d5a12450858d2cd45100012de55
Alfonso <alfonso@viruett.com>
2023-01-20 10:36:20 -0800

    adds admin wallet

--- src/item/item.admins.ts
+++ src/item/item.admins.ts
@@ -3,4 +3,5 @@ export const admins: string[] = [
   "0x09a06f3901f3b0299dd492bd35eA1bB38c5C4c9b", // deployer wallet
   "0x1af8c7140cd8afcd6e756bf9c68320905c355658", // fonzy.eth
   "0x5cD8A628764F35e163B16711Ac415a7C98a877E1", // jyp
+  "0xce90A565D54A8A0b470f71f2f526E90A7c3FAb03", // pax
 ];
