774439f1919920e38841adfc9696d0d361f5b203
alfonsoaranzazu <alfonso@viruett.com>
2023-01-21 14:06:09 -0800

    adds eduz admin wallet

--- src/item/item.admins.ts
+++ src/item/item.admins.ts
@@ -4,4 +4,5 @@ export const admins: string[] = [
   "0x1af8c7140cd8afcd6e756bf9c68320905c355658", // fonzy.eth
   "0x5cD8A628764F35e163B16711Ac415a7C98a877E1", // jyp
   "0xce90A565D54A8A0b470f71f2f526E90A7c3FAb03", // pax
+  "0x42A7e7ad8e8623eb2A3B3bdb2B82F8e0422e5123", // eduz
 ];
