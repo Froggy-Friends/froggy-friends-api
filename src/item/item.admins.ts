16e4fbae5c368d22b02084eeac8c9f24e0724d6d
alfonsoaranzazu <alfonso@viruett.com>
2023-01-27 22:26:06 -0800

    update pax admin wallet

--- src/item/item.admins.ts
+++ src/item/item.admins.ts
@@ -3,6 +3,6 @@ export const admins: string[] = [
   "0x09a06f3901f3b0299dd492bd35eA1bB38c5C4c9b", // deployer wallet
   "0x1af8c7140cd8afcd6e756bf9c68320905c355658", // fonzy.eth
   "0x5cD8A628764F35e163B16711Ac415a7C98a877E1", // jyp
-  "0xce90A565D54A8A0b470f71f2f526E90A7c3FAb03", // pax
+  "0x65e98AE7da7f4d22280A9b6C5d34f70D197Bbc0F", // pax
   "0x42A7e7ad8e8623eb2A3B3bdb2B82F8e0422e5123", // eduz
 ];
