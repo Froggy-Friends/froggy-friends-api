7be00cd4bd85b9c9221f1d00b2d6a47f4c7d2dfe
Fonzy.eth <alfonso@viruett.com>
2023-02-01 10:40:48 -0800

    Updates eduz admin wallet

--- src/item/item.admins.ts
+++ src/item/item.admins.ts
@@ -4,5 +4,5 @@ export const admins: string[] = [
   "0x1af8c7140cd8afcd6e756bf9c68320905c355658", // fonzy.eth
   "0x5cD8A628764F35e163B16711Ac415a7C98a877E1", // jyp
   "0x65e98AE7da7f4d22280A9b6C5d34f70D197Bbc0F", // pax
-  "0x42A7e7ad8e8623eb2A3B3bdb2B82F8e0422e5123", // eduz
+  "0x2b18bDF302a214Bcc7123294b00f50F83D63a731", // eduz
 ];
