d0184a63a428f3c20ad2fab309b137796e9061d3
alfonsoaranzazu <alfonso@viruett.com>
2024-03-16 12:20:39 -0700

    use constants

--- src/contract/contract.service.ts
+++ src/contract/contract.service.ts
@@ -6,33 +6,28 @@ import { Alchemy, Network, OwnedNft } from 'alchemy-sdk';
 
 @Injectable()
 export class ContractService {
-  private alchemyKey: string;
   public alchemy: Alchemy;
-  public alchemyUrl: string;
   public ribbitItems: any;
   public froggyAddress: string;
   public itemsAddress: string;
   public froggySoulboundAdress: string;
-  public ribbitAddress: string;
 
   constructor(private configs: ConfigService) {
-    // addresses
-    this.alchemyKey = this.configs.get<string>('ALCHEMY_API_KEY');
-    this.alchemyUrl = this.configs.get<string>('ALCHEMY_API_URL');
+    // env variables
     this.froggyAddress = this.configs.get<string>('FROGGY_CONTRACT_ADDRESS');
-    this.ribbitAddress = this.configs.get<string>('RIBBIT_CONTRACT_ADDRESS');
     this.itemsAddress = this.configs.get<string>('RIBBIT_ITEM_ADDRESS');
     this.froggySoulboundAdress = this.configs.get<string>('FROGGY_SOULBOUND_ADDRESS');
-    // chain
+    const apiKey = this.configs.get('ALCHEMY_API_KEY');
+    const alchemyUrl = this.configs.get<string>('ALCHEMY_API_URL');
+    const pk = this.configs.get<string>('PRIVATE_KEY');
     const environment = this.configs.get<string>('ENVIRONMENT');
     const network = environment === 'production' ? Network.ETH_MAINNET : Network.ETH_GOERLI;
-    const chainId = environment === 'production' ? 1 : 17000;
+    // provider, signer, contract
+    const alchemyProvider = new ethers.JsonRpcProvider(alchemyUrl);
+    const signer = new ethers.Wallet(pk, alchemyProvider);
+    this.ribbitItems = new ethers.Contract(this.itemsAddress, abiItems, signer);
     // alchemy
-    this.alchemy = new Alchemy({apiKey: this.alchemyKey, network: network});
-    // provider
-    const alchemyProvider = new ethers.AlchemyProvider({ name: network, chainId: chainId }, this.alchemyKey);
-    const wallet = new ethers.Wallet(this.configs.get<string>('PK'), alchemyProvider);
-    this.ribbitItems = new ethers.Contract(this.itemsAddress, abiItems, wallet);
+    this.alchemy = new Alchemy({apiKey, network});
   }
 
   async getFrogOwner(frogId: number): Promise<string> {
